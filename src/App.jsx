9b45781fee887a46476e9f769a0cc04052b3c1b4
Abdul Mubashir <mubashir0356@gmail.com>
2024-06-14 17:00:56 +0530

    setting up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/authService";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1 className="bg-green-400 text-center"> Appwrite</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        console.log(userData, "userData");
+        const payload = { userData };
+        if (userData) {
+          dispatch(login(payload));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return;
+  {
+    loading ? (
+      <p className="text-center">Loading...</p>
+    ) : (
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <main>
+            <h1> Mubashir </h1>
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
