1d0d79ab8a0cd73a083f1d09b60566f406519f63
Abdul Mubashir <mubashir0356@gmail.com>
2024-06-14 10:42:03 +0530

    setting enc variables

---
+++ src/config/envVariable.js
@@ -0,0 +1,9 @@
+const configVariables = {
+    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDBId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
+}
+
+export default configVariables
